931b1bc1bf0527a080e349ca4f21e029d82c8344
M <staycosy@163.com>
2020-05-24 21:48:26 +0800

    compare to T、K、E

--- SPIDemo/src/main/java/com/example/generic/GenericMethod.java
+++ SPIDemo/src/main/java/com/example/generic/GenericMethod.java
@@ -16,8 +16,31 @@ public class GenericMethod {
         return t1;
     }
 
+    public static <K> K add(K k1) {
+        if (k1 instanceof Integer) {
+            System.out.println("String value = " + k1);
+        }
+        if (k1 instanceof String) {
+            System.out.println("Integer value = " + k1);
+        }
+        return k1;
+    }
+
+    // 范型重载T、K、E是同一种类型
+    /*public static <E> E add(E e1,E e2) {
+        if (e1 instanceof String) {
+            System.out.println("E is String " + e1);
+        }
+        if (e1 instanceof Integer) {
+            System.out.println("E is Integer " + e1);
+        }
+        return e1;
+    }*/
+
     public static void main(String[] args) {
         GenericMethod.add(1, 5);
         GenericMethod.add("1", "5");
+        GenericMethod.add("1");
+        GenericMethod.add(1);
     }
 }
